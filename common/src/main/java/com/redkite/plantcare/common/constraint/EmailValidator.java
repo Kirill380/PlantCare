b9a8121850efab897e3ad6ac18e4f53f284e40f1
Kirill Liubun <kreol_123@ukr.net>
2017-02-22 14:24:40 +0200

    Small fix

--- common/src/main/java/com/redkite/plantcare/common/constraint/EmailValidator.java
+++ common/src/main/java/com/redkite/plantcare/common/constraint/EmailValidator.java
@@ -1,12 +1,12 @@
 package com.redkite.plantcare.common.constraint;
 
-import static org.kaaproject.sentinel.dto.utils.ConstraintsConstants.EMAIL_MAX_LENGTH;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
+
+  public static final int EMAIL_MAX_LENGTH = 100;
 
   private static final String pattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*"
           + "@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
