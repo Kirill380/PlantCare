cbfdc211bd3ea6e108b5f31478302aa8aa157068
Kirill Liubun <kreol_123@ukr.net>
2017-05-14 23:59:53 +0300

    Create on start up default roles and users.

--- core-service/src/main/java/com/redkite/plantcare/service/impl/UserServiceImpl.java
+++ core-service/src/main/java/com/redkite/plantcare/service/impl/UserServiceImpl.java
@@ -4,55 +4,119 @@ import com.redkite.plantcare.common.dto.UserList;
 import com.redkite.plantcare.common.dto.UserRequest;
 import com.redkite.plantcare.common.dto.UserResponse;
 import com.redkite.plantcare.convertors.UserConverter;
+import com.redkite.plantcare.dao.RoleDao;
 import com.redkite.plantcare.dao.UserDao;
+import com.redkite.plantcare.model.Role;
 import com.redkite.plantcare.model.User;
 import com.redkite.plantcare.service.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-  @Autowired
-  private UserDao userDao;
+    private PasswordEncoder passwordEncoder;
 
-  @Autowired
-  private UserConverter userConverter;
+    private UserDao userDao;
 
-  @Override
-  @Transactional(isolation = Isolation.REPEATABLE_READ)
-  public UserResponse createUser(UserRequest userRequest) {
-    User user = userConverter.toModel(userRequest);
-    userDao.save(user);
-    return userConverter.toDto(user);
-  }
+    private RoleDao roleDao;
 
-  @Override
-  public UserList getUsers() {
-    return null;
-  }
+    private UserConverter userConverter;
 
-  @Override
-  public UserResponse getUser(Long userId) {
-    return null;
-  }
+    private PlatformTransactionManager txManager;
 
-  @Override
-  public void editUser(UserRequest userRequest) {
+    @Autowired
+    public UserServiceImpl(PasswordEncoder passwordEncoder,
+                           UserDao userDao,
+                           RoleDao roleDao,
+                           UserConverter userConverter,
+                           @Qualifier("transactionManager") PlatformTransactionManager txManager) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDao = userDao;
+        this.roleDao = roleDao;
+        this.userConverter = userConverter;
+        this.txManager = txManager;
+    }
 
-  }
+    private static final String REGULAR_USER_ROLE = "regularUser";
+    private static final String ADMIN_ROLE = "admin";
 
-  @Override
-  @Transactional(isolation = Isolation.REPEATABLE_READ)
-  public void deleteUser(UserRequest userRequest) {
 
-  }
+    @PostConstruct
+    public void init() {
+        TransactionTemplate tmpl = new TransactionTemplate(txManager);
+        tmpl.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                Role regularUser = new Role();
+                regularUser.setName(REGULAR_USER_ROLE);
+                regularUser.setLimitOfPlants(10);
+                regularUser.setLimitOfPrecessingData(1000);
+                roleDao.save(regularUser);
 
-  @Override
-  public boolean checkPasswordMatching(String email, String password) {
-    return false;
-  }
+                Role adminUser = new Role();
+                adminUser.setName(ADMIN_ROLE);
+                adminUser.setLimitOfPlants(-1);
+                adminUser.setLimitOfPrecessingData(-1);
+                roleDao.save(adminUser);
+
+                UserRequest user = new UserRequest();
+                user.setEmail("admin@gmail.com");
+                user.setPassword("admin123");
+                createUser(user, ADMIN_ROLE);
+            }
+        });
+    }
+
+
+    @Override
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public UserResponse createUser(UserRequest userRequest) {
+        return userConverter.toDto(createUser(userRequest, REGULAR_USER_ROLE));
+    }
+
+    private User createUser(UserRequest userRequest, String roleName) {
+        User user = userConverter.toModel(userRequest);
+        user.setCreationDate(LocalDateTime.now());
+        user.setRole(roleDao.findByName(roleName));
+        return userDao.save(user);
+    }
+
+    @Override
+    public UserList getUsers() {
+        return null;
+    }
+
+    @Override
+    public UserResponse getUser(Long userId) {
+        return null;
+    }
+
+    @Override
+    public void editUser(UserRequest userRequest) {
+
+    }
+
+    @Override
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    public void deleteUser(UserRequest userRequest) {
+
+    }
+
+    @Override
+    public boolean checkPasswordMatching(String email, String password) {
+        User user = userDao.findByEmail(email);
+        return passwordEncoder.matches(password, user.getPasswordHash());
+    }
 }
